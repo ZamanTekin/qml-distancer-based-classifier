8d950aa0430c6cdc64b9edf7a8807021434fff58
Zaman Tekin <zamantekin@gmail.com>
2023-03-30 14:14:01 +0100

    test
    test

--- classifier.py
+++ classifier.py
@@ -24,6 +24,7 @@ circuit.class_q = circuit.q[3]
 
 # Creating the circuit
 qc = QuantumCircuit(circuit.q, circuit.cl)
+print("find me here")
 
 # Step A: Hadamard the ancilla and index quibits
 qc.h(circuit.ancilla_q)
